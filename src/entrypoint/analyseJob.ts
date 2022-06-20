a8f6a6257963b031b47386a216caa1e214f9961b
mo-sheikh <mohamed-sheikh1@outlook.com>
2022-06-20 14:00:16 +0100

    fix dependencies - initialise entrypoint

--- src/entrypoint/analyseJob.ts
+++ src/entrypoint/analyseJob.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from "../ports/entrypoint/analyseJobsPort";
+import { Response } from "../ports/entrypoint/analyseJobsPort";
 import { FileSystemRepository } from "../adapters/FileSystemRepository";
 import { analyseJobUseCase } from "../useCase/analyseJobUseCase";
 import { RepositoryError } from "../ports/adapters/repository/RepositoryError";
 
-export const analyseJob = (request: Request): Response => {
+export const analyseJob = (request: any): Response => {
+  console.log("received");
   const repository = new FileSystemRepository("./jobs.json");
   const response = analyseJobUseCase(repository, request);
   if (response instanceof RepositoryError) {
@@ -13,3 +14,5 @@ export const analyseJob = (request: Request): Response => {
   }
   return { status: 200 };
 };
+
+analyseJob(process.argv);
