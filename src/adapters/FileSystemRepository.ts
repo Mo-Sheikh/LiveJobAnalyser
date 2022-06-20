682a85a69f5a5ee36290aa6718c4ff723f3fdd11
mo-sheikh <mohamed-sheikh1@outlook.com>
2022-06-20 13:12:21 +0100

    add error handling for read operation

--- src/adapters/FileSystemRepository.ts
+++ src/adapters/FileSystemRepository.ts
@@ -1,22 +1,36 @@
-import {RepositoryPort, RepositorySchema} from "../ports/adapters/repository/RepositoryPort";
-import {RepositoryError} from "../ports/adapters/repository/RepositoryError";
+import {
+  RepositoryPort,
+  RepositorySchema,
+} from "../ports/adapters/repository/RepositoryPort";
+import {
+  RepositoryError,
+  RepositoryErrorStatusCode,
+} from "../ports/adapters/repository/RepositoryError";
 import * as fs from "fs";
 
 export class FileSystemRepository implements RepositoryPort {
-    path: string;
+  path: string;
 
-    constructor(path: string) {
-        this.path = path;
+  constructor(path: string) {
+    this.path = path;
+  }
+
+  read(): RepositorySchema | RepositoryError {
+    try {
+      const data = fs.readFileSync(this.path, "utf-8");
+      const parsedData = JSON.parse(data);
+      return parsedData;
+    } catch (e) {
+      return new RepositoryError(RepositoryErrorStatusCode.READ_ERROR);
     }
+  }
 
-    read(): RepositorySchema | RepositoryError {
-        const data = fs.readFileSync(this.path, "utf-8")
-        const parsedData = JSON.parse(data)
-        return parsedData
-    }
+  write(_request: string): Promise<"Success" | RepositoryError> {
+    // const currentData = this.read();
 
-    write(_request: string): Promise<"Success" | RepositoryError> {
-        return Promise.resolve("Success")
-
-    }
+    // for (const word of request) {
+    //   currentData[word];
+    // }
+    return Promise.resolve("Success");
+  }
 }
--- tests/integration/adapters/FileSystemRepository.test.ts
+++ tests/integration/adapters/FileSystemRepository.test.ts
@@ -1,22 +1,50 @@
 import * as fs from "fs";
-import {RepositoryPort} from "../../../src/ports/adapters/repository/RepositoryPort";
-import {FileSystemRepository} from "../../../src/adapters/FileSystemRepository";
+import { RepositoryPort } from "../../../src/ports/adapters/repository/RepositoryPort";
+import { FileSystemRepository } from "../../../src/adapters/FileSystemRepository";
+import { RepositoryError } from "../../../src/ports/adapters/repository/RepositoryError";
 
 describe("Given we have a repository", () => {
-    const testPath = "./testOutput.json"
-    let repository: RepositoryPort
+  const testPath = "./testOutput.json";
+  let repository: RepositoryPort;
+  describe("And we want to read from the repo", () => {
     beforeAll(() => {
-        fs.writeFileSync(testPath, `{"test":1}`, 'utf-8')
-        repository = new FileSystemRepository(testPath)
-    })
+      fs.writeFileSync(testPath, `{"test":1}`, "utf-8");
+      repository = new FileSystemRepository(testPath);
+    });
     afterAll(() => {
-        fs.unlinkSync(testPath)
-    })
+      fs.unlinkSync(testPath);
+    });
     it("should read data from the repository", async () => {
-        const data = await repository.read()
+      const data = await repository.read();
 
-        expect(data).toStrictEqual({
-            "test": 1
-        })
-    })
-})
+      expect(data).toStrictEqual({
+        test: 1,
+      });
+    });
+  });
+  // describe("and we want to write to the repo", () => {
+  //   beforeAll(() => {
+  //     fs.writeFileSync(testPath, `{}`, "utf-8");
+  //     repository = new FileSystemRepository(testPath);
+  //   });
+  //
+  //   it("should write into the repo", async () => {
+  //     await repository.write("test");
+  //     const readData = fs.readFileSync(testPath, "utf-8");
+  //     expect(readData).toStrictEqual({ test: 1 });
+  //   });
+  // });
+  describe("Error Handling", () => {
+    let repository: RepositoryPort;
+    describe("Given we have an error when reading from the repo", () => {
+      beforeAll(() => {
+        repository = new FileSystemRepository("path that does not exist");
+      });
+      it("should return a READ_ERROR", () => {
+        const response = repository.read() as RepositoryError;
+        expect(response.name).toBe("Error");
+        expect(response.message).toBe("UNABLE_TO_READ_DATA");
+      });
+    });
+  });
+});
