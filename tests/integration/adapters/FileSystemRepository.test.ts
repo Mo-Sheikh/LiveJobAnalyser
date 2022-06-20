642589fccf892ccdd4a42ee2f4d810e5217c4326
mo-sheikh <mohamed-sheikh1@outlook.com>
2022-06-20 12:59:05 +0100

    add test for read operation on repo layer

--- tests/integration/adapters/FileSystemRepository.test.ts
+++ tests/integration/adapters/FileSystemRepository.test.ts
@@ -1,15 +1,22 @@
 import * as fs from "fs";
 import {RepositoryPort} from "../../../src/ports/adapters/repository/RepositoryPort";
+import {FileSystemRepository} from "../../../src/adapters/FileSystemRepository";
 
-describe("Given we have a repository", ()=>{
+describe("Given we have a repository", () => {
     const testPath = "./testOutput.json"
     let repository: RepositoryPort
-    beforeAll(()=>{
-        fs.writeFileSync(testPath, "{test:1}")
+    beforeAll(() => {
+        fs.writeFileSync(testPath, `{"test":1}`, 'utf-8')
         repository = new FileSystemRepository(testPath)
+    })
+    afterAll(() => {
+        fs.unlinkSync(testPath)
     })
     it("should read data from the repository", async () => {
         const data = await repository.read()
-        expect(data).toBe({test: 1})
+
+        expect(data).toStrictEqual({
+            "test": 1
+        })
     })
 })
