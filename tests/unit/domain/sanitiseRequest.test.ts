41fd4d290db437aa51ffe1417edf12012367c854
mo-sheikh <mohamed-sheikh1@outlook.com>
2022-06-21 15:54:23 +0100

    wip

--- tests/unit/domain/sanitiseRequest.test.ts
+++ tests/unit/domain/sanitiseRequest.test.ts
@@ -4,7 +4,7 @@ describe("Sanitise Request", () => {
   describe("Given we have a valid request", function () {
     it("should sanitise the request", () => {
       const sanitisedRequest = sanitiseRequest("Bad request\n\n(){}");
-      expect(sanitisedRequest).toBe("Bad request");
+      expect(sanitisedRequest).toBe("Bad request  ");
     });
   });
 });
